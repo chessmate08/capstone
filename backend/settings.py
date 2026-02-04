7b77ddc9debcbd6de42eaec4cfa282cb08642870
chessmate08 <jonahbernard96@gmail.com>
2026-02-04 13:19:18 -0700

    ec2_params

--- backend/settings.py
+++ backend/settings.py
@@ -149,6 +149,7 @@ STATIC_URL = '/assets/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'frontend/dist/assets'),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
